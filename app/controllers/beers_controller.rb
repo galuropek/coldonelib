9d8e43028ecb74179d6860bca4a30f2f4f993a01
galuropek <galuropek@gmail.com>
2023-10-24 16:31:18 +0300

    186322963 - add new view

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -4,6 +4,29 @@ class BeersController < ApplicationController
   end
 
   def show
-    @beer = Beer.find(params[:id])
+    @beer = Beer.find params[:id]
+  end
+
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    @beer = Beer.new beer_params
+    if @beer.save
+      redirect_to beers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @beer = Beer.find_by id: params[:id]
+  end
+
+  private
+
+  def beer_params
+    params.require(:beer).permit(:title, :brand, :name, :description)
   end
 end
